37c0e4b17c90a5a46a4adc04f05864967562efb5
glennmatarlo <glennmatarlo99@gmail.com>
2021-11-07 14:55:06 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'progbar';
+  title = 'Progress Bar';
+
+  error: any;
+  currentProg = 0;
+  txtRateValue: any;
+  max = 0;
+  inputMax: any;
+  updateRating(){
+    if(this.txtRateValue <= this.max){
+      this.currentProg = this.txtRateValue;
+    }else{
+      this.error = "Do not exceed maximum value"
+    }
+  }
+
+  enterMax(){
+    if(this.inputMax<=100){
+      this.max = this.inputMax;
+    }else{
+      this.error = "Please enter value 100 below"
+    }
+    
+   
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {FormsModule} from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    FormsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
